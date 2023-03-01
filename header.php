4394ce5f0ebe57e434838ac2349484f764193734
kieuhann <hanb2003783@student.ctu.edu.vn>
2023-03-01 22:48:05 +0700

    Han commit

--- header.php
+++ header.php
@@ -31,3 +31,90 @@
     <!-- Template Stylesheet -->
     <link href="css/style.css" rel="stylesheet">
 </head>
+<body>
+    <div class="container-xxl bg-white p-0">
+        <!-- Spinner Start -->
+        <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+            <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
+                <span class="sr-only">Loading...</span>
+            </div>
+        </div>
+        <!-- Spinner End -->
+
+        <!-- Header Start -->
+        <div class="container-fluid bg-dark px-0">
+            <div class="row gx-0">
+                <div class="col-lg-3 bg-dark d-none d-lg-block">
+                    <a href="index.php" class="navbar-brand w-100 h-100 m-0 p-0 d-flex align-items-center justify-content-center">
+                        <h1 class="m-0 text-primary text-uppercase">FOXIE FLY</h1>
+                    </a>
+                </div>
+                <div class="col-lg-9">
+                    <div class="row gx-0 bg-white d-none d-lg-flex">
+                        <div class="col-lg-7 px-5 text-start">
+                            <div class="h-100 d-inline-flex align-items-center py-2 me-4">
+                                <i class="fa fa-envelope text-primary me-2"></i>
+                                <p class="mb-0">foxiebooking@gmail.com</p>
+                            </div>
+                            <div class="h-100 d-inline-flex align-items-center py-2">
+                                <i class="fa fa-phone-alt text-primary me-2"></i>
+                                <p class="mb-0">+012 345 6789</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-5 px-5 text-end">
+                            <div class="d-inline-flex align-items-center py-2">
+                                <a class="me-3" href=""><i class="fab fa-facebook-f"></i></a>
+                                <a class="me-3" href=""><i class="fab fa-twitter"></i></a>
+                                <a class="me-3" href=""><i class="fab fa-linkedin-in"></i></a>
+                                <a class="me-3" href=""><i class="fab fa-instagram"></i></a>
+                                <a class="" href=""><i class="fab fa-youtube"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <nav class="navbar navbar-expand-lg bg-dark navbar-dark p-3 p-lg-0">
+                        <a href="index.php" class="navbar-brand d-block d-lg-none">
+                            <h1 class="m-0 text-primary text-uppercase">FOXIE BOOKING</h1>
+                        </a>
+                        <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                            <div class="navbar-nav mr-auto py-0">
+                                <?php 
+                                switch ($activate) {
+                                    case 'index':
+                                ?>
+                                <a href="index.php" class="nav-item nav-link active">Trang chủ</a>
+                                <a href="service.php" class="nav-item nav-link">Dịch vụ</a>
+                                <a href="booking.php" class="nav-item nav-link">Đặt vé</a>
+                                <a href="contact.php" class="nav-item nav-link">Liên Hệ</a>
+                                <?php break;
+                                    case 'service':
+                                        ?>
+                                <a href="index.php" class="nav-item nav-link">Trang chủ</a>
+                                <a href="service.php" class="nav-item nav-link active">Dịch vụ</a>
+                                <a href="booking.php" class="nav-item nav-link">Đặt vé</a>
+                                <a href="contact.php" class="nav-item nav-link">Liên Hệ</a>
+                                      <?php break;
+                                    case 'booking': ?>
+                                <a href="index.php" class="nav-item nav-link">Trang chủ</a>
+                                <a href="service.php" class="nav-item nav-link ">Dịch vụ</a>
+                                <a href="booking.php" class="nav-item nav-link active">Đặt vé</a>
+                                <a href="contact.php" class="nav-item nav-link">Liên Hệ</a>
+                                      <?php break;
+                                    case 'contact': ?>
+                                <a href="index.php" class="nav-item nav-link">Trang chủ</a>
+                                <a href="service.php" class="nav-item nav-link ">Dịch vụ</a>
+                                <a href="booking.php" class="nav-item nav-link ">Đặt vé</a>
+                                <a href="contact.php" class="nav-item nav-link active">Liên Hệ</a>
+                                    <?php break;
+                                  }
+                                ?>
+                            </div>
+                            <a href="booking.php" class="btn btn-primary rounded-0 py-4 px-md-5 d-none d-lg-block">Tiến hành đặt vé<i class="fa fa-arrow-right ms-3"></i></a>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+        <!-- Header End -->
