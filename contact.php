57a0b642459174449d971af5a40e2da66e19f0bc
founuptsh <thienthu2428@gmail.com>
2023-03-04 20:34:05 +0700

    Form email

--- contact.php
+++ contact.php
@@ -1,126 +1,157 @@
-
 <?php
 $activate = "contact";
 @include('header.php');
 ?>
-        <!-- Page Header Start -->
-        <div class="container-fluid page-header mb-5 p-0" style="background-image: url(img/carousel-1.jpg);">
-            <div class="container-fluid page-header-inner py-5">
-                <div class="container text-center pb-5">
-                    <h1 class="display-3 text-white mb-3 animated slideInDown"> Contact </h1>
-                    <nav aria-label="breadcrumb">
-                        <ol class="breadcrumb justify-content-center text-uppercase">
-                            <li class="breadcrumb-item"><a href="#">Home</a></li>
-                            <li class="breadcrumb-item"><a href="#">Pages</a></li>
-                            <li class="breadcrumb-item text-white active" aria-current="page">Contact</li>
-                        </ol>
-                    </nav>
+<!-- Page Header Start -->
+<div class="container-fluid page-header mb-5 p-0" style="background-image: url(img/carousel-1.jpg);">
+    <div class="container-fluid page-header-inner py-5">
+        <div class="container text-center pb-5">
+            <h1 class="display-3 text-white mb-3 animated slideInDown"> Contact </h1>
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb justify-content-center text-uppercase">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item"><a href="#">Pages</a></li>
+                    <li class="breadcrumb-item text-white active" aria-current="page">Contact</li>
+                </ol>
+            </nav>
+        </div>
+    </div>
+</div>
+
+<div class="container-xxl py-5">
+    <div class="container">
+        <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
+            <h6 class="section-title text-center text-primary text-uppercase">HERE </h6>
+            <h1 class="mb-5"><span class="text-primary text-uppercase">LIÊN HỆ</span> NHẬN HỖ TRỢ </h1>
+        </div>
+        <div class="row g-4">
+            <div class="col-12">
+                <div class="row gy-4">
+                    <div class="col-md-4">
+                        <h6 class="section-title text-start text-primary text-uppercase">Booking</h6>
+                        <p><i class="fa fa-envelope-open text-primary me-2"></i>foxiebooking@gmail.com</p>
+                    </div>
+                    <div class="col-md-4">
+                        <h6 class="section-title text-start text-primary text-uppercase">Messenger</h6>
+                        <p><i class="fa fa-envelope-open text-primary me-2"></i>foxiebooking@gmail.com</p>
+                    </div>
+                    <div class="col-md-4">
+                        <h6 class="section-title text-start text-primary text-uppercase">CALL</h6>
+                        <p><i class="fa fa-envelope-open text-primary me-2"></i>+012 345 6789</p>
+                    </div>
                 </div>
             </div>
-        </div>
-       
-        <div class="container-xxl py-5">
-            <div class="container">
-                <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
-                    <h6 class="section-title text-center text-primary text-uppercase">HERE </h6>
-                    <h1 class="mb-5"><span class="text-primary text-uppercase">LIÊN HỆ</span> NHẬN HỖ TRỢ </h1>
-                </div>
-                <div class="row g-4">
-                    <div class="col-12">
-                        <div class="row gy-4">
-                            <div class="col-md-4">
-                                <h6 class="section-title text-start text-primary text-uppercase">Booking</h6>
-                                <p><i class="fa fa-envelope-open text-primary me-2"></i>foxiebooking@gmail.com</p>
-                            </div>
-                            <div class="col-md-4">
-                                <h6 class="section-title text-start text-primary text-uppercase">Messenger</h6>
-                                <p><i class="fa fa-envelope-open text-primary me-2"></i>foxiebooking@gmail.com</p>
-                            </div>
-                            <div class="col-md-4">
-                                <h6 class="section-title text-start text-primary text-uppercase">CALL</h6>
-                                <p><i class="fa fa-envelope-open text-primary me-2"></i>+012 345 6789</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6 wow fadeIn" data-wow-delay="0.1s">
-                        <iframe class="position-relative rounded w-100 h-100"
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001156.4288297426!2d-78.01371936852176!3d42.72876761954724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccc4bf0f123a5a9%3A0xddcfc6c1de189567!2sNew%20York%2C%20USA!5e0!3m2!1sen!2sbd!4v1603794290143!5m2!1sen!2sbd"
-                            frameborder="0" style="min-height: 350px; border:0;" allowfullscreen="" aria-hidden="false"
-                            tabindex="0"></iframe>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="wow fadeInUp" data-wow-delay="0.2s">
-                            <form>
-                                <div class="row g-3">
-                                    <div class="col-md-6">
-                                        <div class="form-floating">
-                                            <input type="text" class="form-control" id="name" placeholder="Your Name">
-                                            <label for="name">Your Name</label>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <div class="form-floating">
-                                            <input type="email" class="form-control" id="email" placeholder="Your Email">
-                                            <label for="email">Your Email</label>
-                                        </div>
-                                    </div>
-                                    <div class="col-12">
-                                        <div class="form-floating">
-                                            <input type="text" class="form-control" id="subject" placeholder="Subject">
-                                            <label for="subject">Your address</label>
-                                        </div>
-                                    </div>
-                                    <div class="col-12">
-                                        <div class="form-floating">
-                                            <textarea class="form-control" placeholder="Leave a message here" id="message" style="height: 150px"></textarea>
-                                            <label for="message">Message</label>
-                                        </div>
-                                    </div>
-                                    <div class="col-12">
-                                        <a class="btn-submit btn btn-primary w-100 py-3" type="submit" onclick="event.preventDefault()">Send Message</a>
-                                    </div>
+            <div class="col-md-6 wow fadeIn" data-wow-delay="0.1s">
+                <iframe class="position-relative rounded w-100 h-100"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001156.4288297426!2d-78.01371936852176!3d42.72876761954724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccc4bf0f123a5a9%3A0xddcfc6c1de189567!2sNew%20York%2C%20USA!5e0!3m2!1sen!2sbd!4v1603794290143!5m2!1sen!2sbd"
+                    frameborder="0" style="min-height: 350px; border:0;" allowfullscreen="" aria-hidden="false"
+                    tabindex="0"></iframe>
+            </div>
+            <div class="col-md-6">
+                <div class="wow fadeInUp" data-wow-delay="0.2s">
+                    <form>
+                        <div class="row g-3">
+                            <div class="col-md-6">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" id="name" placeholder="Your Name">
+                                    <label for="name">Your Name</label>
                                 </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Contact End -->
-
-
-        <!-- Newsletter Start -->
-        <div class="container newsletter mt-5 wow fadeIn" data-wow-delay="0.1s">
-            <div class="row justify-content-center">
-                <div class="col-lg-10 border rounded p-1">
-                    <div class="border rounded text-center p-1">
-                        <div class="bg-white rounded text-center p-5">
-                            <h4 class="mb-4">Theo dõi <span class="text-primary text-uppercase">Chuyến bay của chúng tôi</span></h4>
-                            <div class="position-relative mx-auto" style="max-width: 400px;">
-                                <input class="form-control w-100 py-3 ps-4 pe-5" type="text" placeholder="Enter your email">
-                                <button type="button" class="btn btn-primary py-2 px-3 position-absolute top-0 end-0 mt-2 me-2">Submit</button>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-floating">
+                                    <input type="email" class="form-control" id="email" placeholder="Your Email">
+                                    <label for="email">Your Email</label>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" id="subject" placeholder="Subject">
+                                    <label for="subject">Your address</label>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="form-floating">
+                                    <textarea class="form-control" placeholder="Leave a message here" id="message"
+                                        style="height: 150px"></textarea>
+                                    <label for="message">Message</label>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <a class="btn-submit btn btn-primary w-100 py-3" type="submit"
+                                    onclick="event.preventDefault()">Send Message</a>
                             </div>
                         </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Contact End -->
+
+
+<!-- Newsletter Start -->
+<div class="container newsletter mt-5 wow fadeIn" data-wow-delay="0.1s">
+    <div class="row justify-content-center">
+        <div class="col-lg-10 border rounded p-1">
+            <div class="border rounded text-center p-1">
+                <div class="bg-white rounded text-center p-5">
+                    <h4 class="mb-4">Theo dõi <span class="text-primary text-uppercase">Chuyến bay của chúng tôi</span>
+                    </h4>
+                    <div class="position-relative mx-auto" style="max-width: 400px;">
+                        <input class="form-control w-100 py-3 ps-4 pe-5" type="text" placeholder="Enter your email">
+                        <button type="button"
+                            class="btn btn-primary py-2 px-3 position-absolute top-0 end-0 mt-2 me-2">Submit</button>
                     </div>
                 </div>
             </div>
         </div>
-        <!-- Newsletter Start -->
-    
-        <!-- JS FORM START -->
-        <script>
-            const submit = document.querySelector('.btn-submit')
-            const ipName = document.querySelector('#name')
-            const ipEmail = document.querySelector('#email')
-            let temp = true
-            submit.onclick = function(event) {
-                 if(ipName.value.length ==0) temp = false
-                 if(ipEmail.value.length == 0) temp = false
-                 if(temp === true) alert('Gửi thông tin thành công')
-                 else alert('Bạn vui lòng nhập đầy đủ thông tin!')
-                }
-        </script>
-        <!-- JS FORM END -->
+    </div>
+</div>
+<!-- Newsletter Start -->
+
+<!-- JS FORM START -->
+<script>
+const submit = document.querySelector('.btn-submit')
+const ipName = document.querySelector('#name')
+const ipEmail = document.querySelector('#email')
+let temp = true
+submit.onclick = function(event) {
+    if (ipName.value.length == 0) temp = false
+    if (ipEmail.value.length == 0) temp = false
+    if (temp === true) alert('Gửi tin nhắn thành công!')
+    else alert('Bạn vui lòng nhập đầy đủ thông tin!')
+}
+
+// function validateEmail() {
+//     const value = email.value.trim();
+
+//     if (value === '') {
+//         showError(email, 'Email không được bỏ trống');
+//     } else if (!/\S+@\S+\.\S+/.test(value)) {
+//         showError(email, 'Email không hợp lệ');
+//     } else {
+//         clearError(email);
+//         return true;
+//     }
+
+//     return false;
+// }
+</script>
+
+<!-- // fucntion checkEmailunction checkEmail() {
+// var email = document.getElementById('email');
+// var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+// if (!filter.test(email.value)) {
+// alert('Hay nhap dia chi email hop le.\nExample@gmail.com');
+// email.focus;
+// return false;
+// } else {
+// alert('Email nay hop le!');
+// }
+// } -->
+
+<!-- JS FORM END -->
 <?php
 @include('footer.php')
 
