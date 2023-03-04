fe3dde6ba8b6e1ced5acc8df7278d9b1bea7119d
TranThanhKiep2307 <ttkcaptianlp23@gmail.com>
2023-03-04 16:02:12 +0700

    Kiep commit

--- index.php
+++ index.php
@@ -416,23 +416,23 @@ $activate = "index";
                         <i class="fa fa-quote-right fa-3x text-primary position-absolute end-0 bottom-0 me-4 mb-n1"></i>
                     </div>
                     <div class="testimonial-item position-relative bg-white rounded overflow-hidden">
-                        <p>Thành viên</p>
+                        <p>Nhà sáng lập</p>
                         <div class="d-flex align-items-center">
                             <img class="img-fluid flex-shrink-0 rounded" src="img/testimonial-3.jpg" style="width: 45px; height: 45px;">
                             <div class="ps-3">
                                 <h6 class="fw-bold mb-1">Nguyễn Ngọc Kiều Hân</h6>
-                                <small>Tiếp viên hàng không</small>
+                                <small>Thành viên</small>
                             </div>
                         </div>
                         <i class="fa fa-quote-right fa-3x text-primary position-absolute end-0 bottom-0 me-4 mb-n1"></i>
                     </div>
                     <div class="testimonial-item position-relative bg-white rounded overflow-hidden">
-                        <p>Thành viên</p>
+                        <p>Nhà sáng lập</p>
                         <div class="d-flex align-items-center">
-                            <img class="img-fluid flex-shrink-0 rounded" src="img/testimonial-3.jpg" style="width: 45px; height: 45px;">
+                            <img class="img-fluid flex-shrink-0 rounded" src="img/TK.jpg" style="width: 45px; height: 45px;">
                             <div class="ps-3">
                                 <h6 class="fw-bold mb-1">Trần Thanh Kiệp</h6>
-                                <small>Tiếp viên hàng không</small>
+                                <small>Thành viên</small>
                             </div>
                         </div>
                         <i class="fa fa-quote-right fa-3x text-primary position-absolute end-0 bottom-0 me-4 mb-n1"></i>
